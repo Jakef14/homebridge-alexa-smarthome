ac417545faeccb46e18fc9aee05ffaafb82d6017
Jakef14 <jsf02468@gmail.com>
2025-08-16 20:18:00 -0400

    update to local with version bump

--- src/accessory/thermostat-accessory.ts
+++ src/accessory/thermostat-accessory.ts
@@ -94,7 +94,7 @@ export default class ThermostatAccessory extends BaseAccessory {
           this.Characteristic.TargetHeatingCoolingState.OFF,
           this.Characteristic.TargetHeatingCoolingState.HEAT,
           this.Characteristic.TargetHeatingCoolingState.COOL,
-          this.Characteristic.TargetHeatingCoolingState.AUTO,
+          // this.Characteristic.TargetHeatingCoolingState.AUTO,
         ],
       })
       .onGet(this.handleTargetStateGet.bind(this))
